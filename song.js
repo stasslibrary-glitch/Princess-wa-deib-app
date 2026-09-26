f6376ebc169ec29a17837ee9fc0cdb9486e8056e
stasslibrary-glitch <stasslibrary@gmail.com>
2026-09-26 08:01:05 +0300

    Add files via upload

--- song.js
+++ song.js
@@ -31,7 +31,7 @@ const PD_SONGS = [
     {
         title: "Beautiful in White",
         artist: "Westlife Singapore version",
-        file: "songs/Beautiful in White Westlife Singapore.mp3"
+        file: "songs/Beautiful in White.mp3"
     },
     {
         title: "Puzzle of my Heart",
